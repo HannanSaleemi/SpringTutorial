d9d40b06a4f2869f299202038bbac333d282d6cc
Hannan Saleemi <hannan@octopuslabs.com>
2019-12-06 20:16:22 +0000

    PersonDao interface generating and inserting a new Person

---
+++ src/main/java/id/hanntr/demo/dao/PersonDao.java
@@ -0,0 +1,18 @@
+package id.hanntr.demo.dao;
+
+import id.hanntr.demo.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+
+    // Insert person with an ID
+    int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person) {
+        // Generate the random UUID
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+}
