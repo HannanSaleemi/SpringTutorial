07e1c0ce96a3706d8ae7ae3df8a263cb7844b8aa
Hannan Saleemi <hannan@octopuslabs.com>
2019-12-07 06:23:14 +0000

    Setup PostgresDataSource and setup credentails in application.yml

--- src/main/java/datasource/PostgresDataSource.java
+++ src/main/java/datasource/PostgresDataSource.java
@@ -1,4 +1,22 @@
 package datasource;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PostgresDataSource {
+
+    // Instantiating this HikariDataSource as a Bean and then create a configuration file containing the credentials
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
 }
