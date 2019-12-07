19d45e7aad84ed2c2c9d64fd75cc76a69bd674f0
Hannan Saleemi <hannan@octopuslabs.com>
2019-12-07 04:21:42 +0000

    Create the REST resource and set AddPerson to a POST Request

--- src/main/java/id/hanntr/demo/api/PersonController.java
+++ src/main/java/id/hanntr/demo/api/PersonController.java
@@ -3,7 +3,11 @@ package id.hanntr.demo.api;
 import id.hanntr.demo.model.Person;
 import id.hanntr.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+// Make REST Resource
+@RestController
 public class PersonController {
 
     private final PersonService personService;
@@ -14,6 +18,8 @@ public class PersonController {
     }
 
     // Add a method that adds a person
+    // Set POST
+    @PostMapping
     public void addPerson(Person person) {
         personService.addPerson(person);
     }
